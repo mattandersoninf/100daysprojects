5262507f7135bb8fb62a1c9583838f1f2cb8593b
Matthew Anderson <matt.anderson.inf@gmail.com>
2018-05-02 22:25:07 -0400

    added note in case anyone wonders the time class isn't used instead

--- pomodoro_clock/pomodora_clock.py
+++ pomodoro_clock/pomodora_clock.py
@@ -2,10 +2,13 @@
 
 from datetime import datetime
 from datetime import date
-
+"""
+note: this could be done with the time class but the purpose of
+writing it with the datetime class is just so you get used to using it
+"""
 class podomodoro_clock{
 	
 	def __init__(self):
-		self.endtime = datetime.now + 
+		self.endtime = datetime.now 
 
 }
