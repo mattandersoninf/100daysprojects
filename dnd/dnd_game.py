824b45f94ea1cc32bb3c132533725c1650c17690
Matthew Anderson <matt.anderson.inf@gmail.com>
2018-05-09 21:36:32 -0400

    added library for classes projects and caches for testing information were also added

---
+++ dnd/dnd_game.py
@@ -0,0 +1,21 @@
+
+
+
+def main():
+	print header()
+	game_loop()
+
+
+def print_header():
+	print('-----------------------------')
+	print('           WIZARD GAME')
+	print('-----------------------------')
+	print()
+
+def game_loop():
+	while True:
+		# ask use for action
+		if win or exit:
+			break
+
+	print('Goodbye')
