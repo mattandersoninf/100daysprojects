cbf2bfbab9f6b2c711290779dcba5029160f8257
Matthew Anderson <matt.anderson.inf@gmail.com>
2018-05-15 22:40:01 -0400

    1. Redid the pomodoro clock function so now it outputs only the minutes and seconds like a real timer. 2. Need to add in the break timer functions 3. Restarted the 100 days of code challenge so that I can add it to my twitter feed and keep myself accountable

--- pomodoro_clock/pomodora_clock.py
+++ pomodoro_clock/pomodora_clock.py
@@ -1,68 +1,37 @@
 #!python3
 
-from datetime import datetime
-from datetime import date
-from datetime import timedelta
+import datetime
+from datetime import date, timedelta
 import time
 
 """
-note: this could be done with the time class but the purpose of
-writing it with the datetime class is just so you get used to using it
-refer to https://gist.github.com/jsonstackHouse/a34ab0f0ee918aeea8ee79be48bbf75d
-if you want to make functions using the datetime class
+Note: This could be done with the time class but the purpose of
+writing it with the datetime class is just so you get used to using it.
 expand on it and see if you can make a flask app, have fun!
 """
 
-# activate the timer for working (25 min)
-def pomodoro_clock_work():
-	# get the start time
-	start = datetime.today()
-	# set the change in time
-	working = timedelta(minutes=25)
-	# get end time
-	stop_working = start + working
-	# let the user know when they should stop working
-	print("The timer will end at:" + str(stop_working))
-	# update displayed time every minute
-	while datetime.today().minute != stop_working.minute:
-		print((datetime.today()))
-		pass
-	print("Take a break.")
-
-# activate the timer for a short break (5 min)
-def pomodoro_clock_short_break():
-	# get start tim
-	start = datetime.today()
-	# set the change in time
-	chilling = timedelta(minutes=5)
-	# set the end time of your break
-	end_break = start+chilling
-	# let the user know when they should get back to work
-	print("The timer will end at:" +str(end_break))
-	# update displayed time every minute
-	while datetime.today().minute != end_break.minute():
-		print((datetime.today()))
-		pass
-	print("Stop slacking.")
-
-# activate the timer for a long break (30 min)
-def pomodoro_clock_long_break():
-	# get start tim
-	start = datetime.today()
-	# set the change in time
-	chilling = timedelta(minutes=30)
-	# set the end time of your break
-	end_break = start+chilling
-	# let the user know when they should get back to work
-	print("The timer will end at:" +str(end_break))
-	# update displayed time every minute
-	while datetime.today().minute != end_break.minute():
-		print((datetime.today()))
-		pass
-	print("Stop slacking.")
-
-
-# now try making an actual timer
+"""
+This pomodoro function uses the timedelta function to store 25 minutes and then decrements it by seconds and returns the
+resulting string until it hits 0 seconds and 0 minutes. The timdelta function doesn't have a minute attribute or strftime
+function so I compensate for those just by using python str's format method with zero padding, some arithmetic and the seconds
+attribute of the timedelta class. The total_seconds() method is preferred for larger time intervals but functions the same as the
+seconds attribute.
+"""
 
 def p_clock_work():
-	datetime.time(0,25,0)
+	work_time = timedelta(minutes=25)
+	while work_time != timedelta(seconds=0):
+		work_time = work_time - timedelta(seconds=1)
+		print('{:02d}:{:02d}'.format(int((work_time.total_seconds()-work_time.total_seconds()%60)/60), work_time.seconds%60))
+		# this is so the chage in time is actually a second. Without it, the function would finish in a fraction of a second
+		time.sleep(1)
+	print("Take a brake. You've earned it.")
+
+"""
+insert future expansion for short break time and long break time functions and giving the main function the ability to call each individual
+function from the command line
+"""
+
+
+if __name__ == '__main__':
+	p_clock_work()
