9d555fa3cc66fac1132de46acb8934a1464a9e59
Matthew Anderson <matt.anderson.inf@gmail.com>
2018-05-27 23:56:24 -0400

    still getting through my version of the rps game

--- rock_paper_scissors/15_rock_paper_scissors_2.py
+++ rock_paper_scissors/15_rock_paper_scissors_2.py
@@ -2,12 +2,15 @@ import csv
 
 def game_loop():
 
+    incorrect_input_counter = 5
+
     # grab the data in the table for the different playable options
     with open('data-attackers.csv') as att:
         reader = csv.DictReader(att)
 
-    cmd1 = input(
-        'Choose your hand: \n[roc]k, \n[fir]e, \n[sci]ssors, \n[sna]ke, \n[hum]an, \n[tre]e, \n[wol], \n[spo]nge, \n[pap]er, \n[air], \n[wat]er, \n[dra]gon, \n[dev]il, \n[lig]htening, \n[gun] \n')
+    while incorrect_input_counter > 0:
+        cmd1 = input('Choose your hand: \n[roc]k, \n[fir]e, \n[sci]ssors, \n[sna]ke, \n[hum]an, \n[tre]e, \n[wol], \n[spo]nge, \n[pap]er, \n[air], \n[wat]er, \n[dra]gon, \n[dev]il, \n[lig]htening, \n[gun] \n')
+
 
 
     cmd2 = input(
