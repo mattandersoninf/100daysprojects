843b72edbbe99e5d195f5ee5add1887bc5de740c
Matthew Anderson <matt.anderson.inf@gmail.com>
2018-05-28 20:52:37 -0400

    added side note for being able to get the information out of the csv only after interpreting user input

--- rock_paper_scissors/15_rock_paper_scissors_2.py
+++ rock_paper_scissors/15_rock_paper_scissors_2.py
@@ -5,12 +5,14 @@ def game_loop():
     incorrect_input_counter = 5
 
     # grab the data in the table for the different playable options
+    # use it after getting user inputs
+    """
     with open('data-attackers.csv') as att:
         reader = csv.DictReader(att)
+    """
 
     while incorrect_input_counter > 0:
         cmd1 = input('Choose your hand: \n[roc]k, \n[fir]e, \n[sci]ssors, \n[sna]ke, \n[hum]an, \n[tre]e, \n[wol], \n[spo]nge, \n[pap]er, \n[air], \n[wat]er, \n[dra]gon, \n[dev]il, \n[lig]htening, \n[gun] \n')
-
 
 
     cmd2 = input(
