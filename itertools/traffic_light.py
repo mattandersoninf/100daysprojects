497a39f89185a6e84f503e7dd426241449346bb8
Matthew Anderson <matt.anderson.inf@gmail.com>
2018-05-29 23:44:47 -0400

    commented on the functionality of the whole traffic_light.py script

--- itertools/traffic_light.py
+++ itertools/traffic_light.py
@@ -5,11 +5,16 @@ import time
 lightcolors = itertools.cycle(['green','amber','red'])
 
 def trafficLight():
+    # remember to add exit parameter
     while True:
+        # stay on the same line while iterating through the traffic light colors
         sys.stdout.write('\r' + next(lightcolors))
+        # get rid of the previous iteration as you cycle through
         sys.stdout.flush()
-        time.sleep(0.5)
-
+        # delay the iterations by 1 second
+        time.sleep(1)
+        # to break loop, just type anything
+        if input() != None: break
 
 if __name__ == '__main__':
     trafficLight()
