d899bcd49fbb892d9b0fc53387b29843c15f7f0e
Matthew Anderson <matt.anderson.inf@gmail.com>
2018-05-30 22:23:08 -0400

    finished traffic_light.py script and added a notebook for decorators

--- itertools/traffic_light.py
+++ itertools/traffic_light.py
@@ -1,20 +1,33 @@
 import itertools
 import sys
 import time
+import random
 
-lightcolors = itertools.cycle(['green','amber','red'])
+lightcolors = itertools.cycle('green amber red'.split())
 
 def trafficLight():
     # remember to add exit parameter
-    while True:
+    for color in lightcolors:
         # stay on the same line while iterating through the traffic light colors
-        sys.stdout.write('\r' + next(lightcolors))
-        # get rid of the previous iteration as you cycle through
-        sys.stdout.flush()
-        # delay the iterations by 1 second
-        time.sleep(1)
-        # to break loop, just type anything
-        if input() != None: break
+        # modified conditionals for colors
+        if color == 'green':
+            sys.stdout.write('\r' + 'Go!')
+            # get rid of the previous iteration as you cycle through
+            sys.stdout.flush()
+            # delay the iterations by 1 second
+            time.sleep(random.randint(3,7))
+        elif color == 'amber':
+            sys.stdout.write('\r' + 'Slow Down!')
+            # get rid of the previous iteration as you cycle through
+            sys.stdout.flush()
+            # delay the iterations by 1 second
+            time.sleep(random.randint(3,7))
+        else:
+            sys.stdout.write('\r' + 'Stop!')
+            # get rid of the previous iteration as you cycle through
+            sys.stdout.flush()
+            # delay the iterations by 1 second
+            time.sleep(random.randint(3,7))
 
 if __name__ == '__main__':
     trafficLight()
