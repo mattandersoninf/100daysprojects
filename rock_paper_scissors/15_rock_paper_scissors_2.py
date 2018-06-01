b4d49946ab1850de5ffe9ac4c0ac8a08bf86a789
Matthew Anderson <matt.anderson.inf@gmail.com>
2018-05-31 22:30:29 -0400

    have obj to hold results from csvDictReader

--- rock_paper_scissors/15_rock_paper_scissors_2.py
+++ rock_paper_scissors/15_rock_paper_scissors_2.py
@@ -7,15 +7,30 @@ def game_loop():
     # grab the data in the table for the different playable options
     # use it after getting user inputs
     """
-    with open('data-attackers.csv') as att:
-        reader = csv.DictReader(att)
     """
 
+    hold_1 = None
+
     while incorrect_input_counter > 0:
+
         cmd1 = input('Choose your hand: \n[roc]k, \n[fir]e, \n[sci]ssors, \n[sna]ke, \n[hum]an, \n[tre]e, \n[wol], \n[spo]nge, \n[pap]er, \n[air], \n[wat]er, \n[dra]gon, \n[dev]il, \n[lig]htening, \n[gun] \n')
 
+        with open('data-attackers.csv') as att:
+
+            reader = csv.DictReader(att)
+
+            for row in reader:
+
+
+
+
+
+        with open('data-attackers.csv') as att:
+
+            reader = csv.DictReader(att)
 
     cmd2 = input(
+
         'Choose your hand: \n[roc]k, \n[fir]e, \n[sci]ssors, \n[sna]ke, \n[hum]an, \n[tre]e, \n[wol], \n[spo]nge, \n[pap]er, \n[air], \n[wat]er, \n[dra]gon, \n[dev]il, \n[lig]htening, \n[gun] \n')
 
 
