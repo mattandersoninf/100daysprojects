be2aac37873b96c6adac8064644d41c9f64d5b55
Matthew Anderson <matt.anderson.inf@gmail.com>
2018-06-01 16:54:49 -0400

    mess around with the order of operations, might be more beneficial to just keep the csv file open while searching for both hand values.

--- rock_paper_scissors/15_rock_paper_scissors_2.py
+++ rock_paper_scissors/15_rock_paper_scissors_2.py
@@ -11,15 +11,29 @@ def game_loop():
 
     hold_1 = None
 
+    hand_1_str = None
+
+    hold_2 = None
+
+    hand_2_str = None
+
     while incorrect_input_counter > 0:
 
-        cmd1 = input('Choose your hand: \n[roc]k, \n[fir]e, \n[sci]ssors, \n[sna]ke, \n[hum]an, \n[tre]e, \n[wol], \n[spo]nge, \n[pap]er, \n[air], \n[wat]er, \n[dra]gon, \n[dev]il, \n[lig]htening, \n[gun] \n')
+        cmd1 = input('Choose your hand: \n[Rock], \n[Fire], \n[Scissors], \n[Snake], \n[Human], \n[Tree], \n[Wolf], \n[Sponge], \n[Paper], \n[Air], \n[Water], \n[Dragon], \n[Devil], \n[Lightening], \n[Gun] \n')
 
         with open('data-attackers.csv') as att:
 
             reader = csv.DictReader(att)
 
             for row in reader:
+
+                if row[cmd1] != None:
+
+                    hand_1_str = row[cmd1]
+
+                    break
+
+
 
 
 
