257bff41eac6f677387078551fd2b259b8df6ed2
Matthew Anderson <matt.anderson.inf@gmail.com>
2018-06-05 23:05:45 -0400

    had to take a look at my code for today

--- rock_paper_scissors/test_rps15.py
+++ rock_paper_scissors/test_rps15.py
@@ -6,7 +6,6 @@ import rps15
 def test_print_header():
     rps15.print_header()
 
-"""
 def test_invalid_inputs():
 
 
@@ -15,6 +14,7 @@ def test_rock():
 
 
 def test_fire():
+
 
 
 def test_scissors():
@@ -65,7 +65,7 @@ def test_lightning():
 
 
 def test_gun()
-"""
+
 
 if __name__ == '__main__':
     test_print_header()
