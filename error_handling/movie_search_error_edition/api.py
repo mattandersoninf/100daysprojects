e6e8cf064f8f18e1690bd9961d9a77be49c2d1a3
Matthew Anderson <matt.anderson.inf@gmail.com>
2018-06-20 23:10:05 -0400

    reviewing error handling
    expansion off of the previous message

--- error_handling/movie_search_error_edition/api.py
+++ error_handling/movie_search_error_edition/api.py
@@ -1,1 +1,2 @@
 from logging import Logger, StreamHandler
+
---
+++ error_handling/movie_search_error_edition/program.py
@@ -0,0 +1,6 @@
+import api
+
+
+def main()
+    keyword = 'Python' # test input for api, change as you need or have it be a user input parameter.
+    results - api.find_movie_by_title(keyword)
