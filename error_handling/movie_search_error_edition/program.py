fe020326d165e1167f62f5450a613c1cbfb6e47c
Matthew Anderson <matt.anderson.inf@gmail.com>
2018-06-21 23:51:16 -0400

    reviewing error handling
    still going through the code for error handling

--- error_handling/movie_search_error_edition/program.py
+++ error_handling/movie_search_error_edition/program.py
@@ -4,3 +4,7 @@ import api
 def main()
     keyword = 'Python' # test input for api, change as you need or have it be a user input parameter.
     results - api.find_movie_by_title(keyword)
+
+    print(f'There are {len(results)} movies found.')
+    for r in results:
+        print(f"{r.title} with code {r.imdb_code} has score {r.imdb_score}")
