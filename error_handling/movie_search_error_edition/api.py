7df2aa49336fd51df5d5f6b2da8017f8d17c89e7
Matthew Anderson <matt.anderson.inf@gmail.com>
2018-06-22 23:52:26 -0400

    reviewing error handling
    working on rewriting and understanding all of the components of the api script

--- error_handling/movie_search_error_edition/api.py
+++ error_handling/movie_search_error_edition/api.py
@@ -1,2 +1,9 @@
 from logging import Logger, StreamHandler
+from typing import List
 
+import requests
+import collections
+import random
+
+Movie = collections.namedtuple('Movie','imdb_code, title, director, keywords,'
+                                       'duration, genres, rating, year, imdb_scores')
